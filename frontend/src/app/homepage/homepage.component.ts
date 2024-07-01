f4a8c97708ef5c6fd2c15429d72205f4a3fcfc57
Andrei Ulinici <aulinici@Lenovo.com>
2024-07-01 15:35:57 +0300

    [Andrei] Add Homepage with mock post

--- frontend/src/app/homepage/homepage.component.ts
+++ frontend/src/app/homepage/homepage.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
-  styleUrl: './homepage.component.scss'
+  styleUrl: './homepage.component.scss',
 })
 export class HomepageComponent {
+  username = 'Andrei';
+  userAvatarUrl =
+    'https://aui.atlassian.com/aui/9.1/docs/images/avatar-person.svg';
 
+  constructor() {}
+
+  getUser() {}
+
+  getPosts() {}
 }
